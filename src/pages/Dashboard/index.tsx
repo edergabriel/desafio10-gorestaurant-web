edee8d6db38367acb2ca20611c5951e4dfb58c96
Eder Gabriel <eder1602@outlook.com>
2020-06-15 23:21:32 -0300

    Finalização do desafio 10

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -58,11 +58,28 @@ const Dashboard: React.FC = () => {
   async function handleUpdateFood(
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
-    // TODO UPDATE A FOOD PLATE ON THE API
+    const newFoodList = foods.map(currentFood => {
+      if(currentFood.id !== editingFood.id) {
+        return currentFood;
+      }
+      return {
+        ... food,
+        id: editingFood.id,
+        available: editingFood.available,
+      };
+    });
+    setFoods(newFoodList);
+    await api.put(`/foods/${editingFood.id}`, {
+      ... food,
+      id: editingFood.id,
+      available: editingFood.available,
+    })
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`/foods/${id}`);
+    const newFoodList = foods.filter(currentFood => currentFood.id !== id);
+    setFoods(newFoodList);
   }
 
   function toggleModal(): void {
@@ -74,7 +91,8 @@ const Dashboard: React.FC = () => {
   }
 
   function handleEditFood(food: IFoodPlate): void {
-    // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    setEditingFood(food);
+    toggleEditModal();
   }
 
   return (
